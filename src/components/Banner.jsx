fb554a74b9bf25c54f4ceb22f83f84016d54dffc
Pedrenri <phenrigoncalves@gmail.com>
2023-06-28 22:51:02 -0300

    Release 2.0.0

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -87,18 +87,18 @@ export const Banner = () => {
                 initial={{ x: -50, opacity: 0 }}
                 whileInView={{ x: 0, opacity: 1 }}
                 transition={{ delay: 0.25 }}
-                className="text-xl text-center"
+                className="text-2xl md:text-xl text-center banner-sub"
               >
-                FULLSTACK, DESIGNER & FOCADO EM EVOLUIR.
+                FULLSTACK DEVELOPER <span>&</span> DESIGNER
               </motion.p>
               <motion.a
                 href="#connect"
                 initial={{ y: 50, opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
               >
-                <button onClick={() => console.log("connect")}>
+               {/*  <button onClick={() => console.log("connect")}>
                   Fale Comigo! <ArrowDownCircle size={25} />
-                </button>
+                </button> */}
               </motion.a>
             </div>
           </Col>
@@ -131,17 +131,14 @@ export const Banner = () => {
           </Col> */}
         </Row>
       </Container>
-      <div className="custom-shape-divider-bottom-1666304608 z-20">
+      <div class="custom-shape-divider-bottom-1687571069">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
           preserveAspectRatio="none"
         >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
-          ></path>
+          <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
         </svg>
       </div>
     </section>
