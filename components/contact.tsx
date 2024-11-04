999bad66ce1e4b2b3b91a2a3bbce200fdba80060
Pedro Henri <99422315+Pedrenri@users.noreply.github.com>
2024-11-04 19:24:16 -0300

    feat: ajuste id

--- components/contact.tsx
+++ components/contact.tsx
@@ -52,8 +52,8 @@ export const Contact = memo(() => {
 
   return (
     <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 id="contact" className="relative pb-4 z-10 text-3xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+      <div className="max-w-2xl mx-auto p-4" id="contact">
+        <h1  className="relative pb-4 z-10 text-3xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
           {t('Title')}
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10 pb-4">
